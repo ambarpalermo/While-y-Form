a75504e175a72899ff1d3f2ebdbe089af10a3b9c
ambarpalermo <ambarpalermo06@gmail.com>
2022-04-26 09:05:53 -0300

    Update button.cs
    finish

--- Assets/button.cs
+++ Assets/button.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class button : MonoBehaviour
 {
+    public int num1;
+    public int num2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +20,17 @@ public class button : MonoBehaviour
     }
     public void TocaAca()
     {
-        Debug.Log("Hola Mundo");
+        if (num1 >= num2 )
+        {
+            Debug.Log("Error, revisar los numeros ingresados");
+        }
+        else
+        {
+            while (num1 <= num2)
+            {
+                Debug.Log(num2);
+                num2--;
+            }
+        }
     }
 }
